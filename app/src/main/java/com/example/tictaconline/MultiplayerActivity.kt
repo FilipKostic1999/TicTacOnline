0b6f41897ddf46b28f11b2434cf12e567a344086
alex88g <alexander.gallorini@gmail.com>
2023-02-25 20:51:58 +0100

    rebuilding main activity

--- app/src/main/java/com/example/tictaconline/MultiplayerActivity.kt
+++ app/src/main/java/com/example/tictaconline/MultiplayerActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tictaconline
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Message
@@ -25,12 +26,17 @@ class MultiplayerActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMultiplayerBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMultiplayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
         initBoard()
     }
+
 
     private fun initBoard() {
 
@@ -44,6 +50,9 @@ class MultiplayerActivity : AppCompatActivity() {
         boardList.add(binding.c2)
         boardList.add(binding.c3)
     }
+
+
+
 
     fun boardTaped(view: View) {
 
