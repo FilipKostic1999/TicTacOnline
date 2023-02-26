c6891752ac35cb5131b40f6c3d38f8f331713359
alex88g <alexander.gallorini@gmail.com>
2023-02-26 01:42:09 +0100

    statistics score done

---
+++ app/src/main/java/com/example/tictaconline/StatsActivity.kt
@@ -0,0 +1,42 @@
+package com.example.tictaconline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+
+class StatsActivity : AppCompatActivity() {
+
+
+    lateinit var textViewP1 : TextView
+    lateinit var textViewP2 : TextView
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stats)
+
+
+
+
+
+        textViewP1 = findViewById(R.id.Stats1)
+        textViewP2 = findViewById(R.id.Stats2)
+
+
+        val shared = getSharedPreferences("Score", MODE_PRIVATE)
+        val Score = shared.getInt("Score", 0)
+
+        Log.d("!!!", "score:  $Score" )
+
+
+        val shared2 = getSharedPreferences("Score2", MODE_PRIVATE)
+        val Score2 = shared2.getInt("Score2", 0)
+
+        textViewP1.text = "Player1 wins: ${Score}"
+        textViewP2.text = "Player2 wins: ${Score2}"
+
+    }
+}
